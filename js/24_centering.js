7fbd67c9f1fefac8430478f9e62b09c7a9afb667
Aakash Deep <7773aakashsharma@gmail.com>
2024-06-20 14:05:22 +0530

    co-ordinates

---
+++ js/24_centering.js
@@ -0,0 +1,6 @@
+const field = document.getElementById('field');
+const ball = document.getElementById('ball');
+
+// Center the ball within the field
+ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px';
+ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px';
